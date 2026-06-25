888cdc20af9151379a40e88cad9e1d157798e04e
Will Parkinson <will.j.parkinson@pm.me>
2026-06-25 14:35:05 +0700

    Formatted code.

--- javaScript.js
+++ javaScript.js
@@ -1,28 +1,29 @@
-const gridSize = 10;
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let grid = [];
+main();
 
-/*Generate matrix with random letters*/
-for (let i = 0; i < gridSize; i++) {
-    grid[i] = [];
-    for (let j = 0; j < gridSize; j++) {
-        grid[i][j] = {
-            letter: characters.charAt(Math.floor(Math.random()*characters.length)),
-            n: null,
-            partOfWord: false,
-            s: false
+/*Sets up the grid and calls functions for adding words and painting the grid on the canvas*/
+function main(){
+    const gridSize = 10;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let i = 0; i < gridSize; i++) {
+        grid[i] = [];
+        for (let j = 0; j < gridSize; j++) {
+            grid[i][j] = {
+                letter: characters.charAt(Math.floor(Math.random()*characters.length)),
+                n: null,
+                partOfWord: false,
+                s: false
+            }
         }
     }
+    let words = ["HELLO","GOODBYE","ATEST","ANOTHER"];
+    let n = 0;
+    words.forEach((value) => {
+        addWord(value,[true,true,true,true,true,true,true,true],n);
+        n++;
+    })
+    makeWordSearch();
 }
-
-let words = ["HELLO","GOODBYE","ATEST","ANOTHER"];
-let n = 0;
-words.forEach((value) => {
-    addWord(value,[true,true,true,true,true,true,true,true],n);
-    n++;
-})
-
-
 
 /*Adds words to wordsearch*/
 function addWord(word,directions,n){
@@ -39,7 +40,7 @@ function addWord(word,directions,n){
     /*Calls function to check if the word will fit in the grid 50 times*/
     function setupWord(){
         let counter = 0;
-        while(!checkWord(Math.floor(Math.random()*gridSize),Math.floor(Math.random()*gridSize),setDirection()) && counter < 50){
+        while(!checkWord(Math.floor(Math.random()*grid.length),Math.floor(Math.random()*grid.length),setDirection()) && counter < 50){
             counter++;
         }
     }
@@ -48,7 +49,7 @@ function addWord(word,directions,n){
         let c = {x:xStart,y:yStart};
         let workOk = true;
         for(i=0;i<word.length;i++){
-            if(c.x >= gridSize || c.x<0 || c.y >= gridSize || c.y<0 || grid[c.y][c.x].partOfWord == true || ((i!=(word.length-1)) && diagonalCross(c.x,c.y,direction))){
+            if(c.x >= grid.length || c.x<0 || c.y >= grid.length || c.y<0 || grid[c.y][c.x].partOfWord == true || ((i!=(word.length-1)) && diagonalCross(c.x,c.y,direction))){
                 workOk = false;
                 break;
             }
@@ -61,11 +62,12 @@ function addWord(word,directions,n){
             return false;
         }
     }
+    /*Prevents diagonal words from crossing*/
     function diagonalCross(x,y,direction){
         switch(direction){ 
             default: return false;
             case 1: 
-                if(x+1<gridSize && y-1>=0 && grid[y][x+1].partOfWord == true && grid[y-1][x].partOfWord == true && grid[y][x+1].n == grid[y-1][x].n){
+                if(x+1<grid.length && y-1>=0 && grid[y][x+1].partOfWord == true && grid[y-1][x].partOfWord == true && grid[y][x+1].n == grid[y-1][x].n){
                     return true;
                 } else {
                     return false;
@@ -77,13 +79,13 @@ function addWord(word,directions,n){
                     return false;
                 }    
             case 5: 
-                if(x-1>=0 && y+1<gridSize && grid[y][x-1].partOfWord == true && grid[y+1][x].partOfWord == true && grid[y][x-1].n == grid[y+1][x].n){
+                if(x-1>=0 && y+1<grid.length && grid[y][x-1].partOfWord == true && grid[y+1][x].partOfWord == true && grid[y][x-1].n == grid[y+1][x].n){
                     return true;
                 } else {
                     return false;
                 }    
             case 7: 
-                if(x+1<gridSize && y+1<gridSize && grid[y][x+1].partOfWord == true && grid[y+1][x].partOfWord == true && grid[y][x+1].n == grid[y+1][x].n){
+                if(x+1<grid.length && y+1<grid.length && grid[y][x+1].partOfWord == true && grid[y+1][x].partOfWord == true && grid[y][x+1].n == grid[y+1][x].n){
                     return true;
                 } else {
                     return false;
@@ -127,25 +129,29 @@ function addWord(word,directions,n){
     }
 }
 
-makeWordSearch(grid,words.length);
-function makeWordSearch(grid,nOfWords){
+/*Draws the completed grid on the canvas*/
+function makeWordSearch(){
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
     ctx.font = "60px Arial";
     const start = 50;
     let c = {x:start,y:start};
-    let drawWord = [[],[],[],[]]
+    let drawWord = [];
     let letters = []
     for(j=0;j<grid.length;j++){
         for(i=0;i<grid[j].length;i++){
             letters.push([grid[j][i].letter,c.x,c.y]);
             if(grid[j][i].s){
-                drawWord[grid[j][i].n].push(c.x,c.y);
+                if(!drawWord[grid[j][i].n]){
+                    drawWord[grid[j][i].n] = [c.x,c.y];
+                } else{
+                    drawWord[grid[j][i].n].push(c.x,c.y);
+                }
             }
-            c.x = c.x+ 60;
+            c.x += 60;
         }
         c.x = start;
-        c.y = c.y + 60;
+        c.y += 60;
     }
     drawWord.forEach((value) => {
         addColour(value[0],value[1],value[2],value[3]);
